80fd926a5fc73a8616750089401bd35e1000e3fc
rjblee <ralph.lee@live.ca>
2025-05-05 02:04:50 +0900

    Add: WidgetsFlutterBinding method

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:carrot_market/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'src/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
