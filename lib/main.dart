ef904c525464e4b6788a34737a7674517e24930d
rjblee <ralph.lee@live.ca>
2025-05-08 23:54:23 +0900

    Add: prefs variable and initiate shared preferences instance

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,14 @@ import 'package:carrot_market/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'src/app.dart';
 
+late SharedPreferences prefs;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  prefs = await SharedPreferences.getInstance();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
