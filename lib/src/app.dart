b74424d8c1803272b3c9501d6f1a7ac308e73e7c
rjblee <ralph.lee@live.ca>
2025-05-14 23:58:27 +0900

    Add: Determine whether the page has been initiated already or not

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,27 @@
+import 'package:carrot_market/src/init/page/init_start_page.dart';
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+import '../main.dart';
+import 'splash/page/splash_page.dart';
+
+class App extends StatefulWidget {
   const App({super.key});
 
   @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  late bool isInitStarted;
+
+  @override
+  void initState() {
+    super.initState();
+    isInitStarted = prefs.getBool('isInitStarted') ?? true;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Image.asset('assets/images/logo_simbol.png')),
-    );
+    return isInitStarted ? const InitStartPage() : const SplashPage();
   }
 }
