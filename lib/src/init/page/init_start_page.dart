84e2d39d628ad2d26193daa3c2c44d5755b4b694
rjblee <ralph.lee@live.ca>
2025-05-16 22:03:48 +0900

    Add: logo image and text

--- lib/src/init/page/init_start_page.dart
+++ lib/src/init/page/init_start_page.dart
@@ -1,13 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class InitStartPage extends StatelessWidget {
   const InitStartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('초기 소개 페이지', style: TextStyle(color: Colors.white)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 99,
+              height: 116,
+              child: Image.asset('assets/images/logo_simbol.png'),
+            ),
+            const SizedBox(height: 40),
+            Text(
+              '당신 근처의 밤톨마켓',
+              style: GoogleFonts.notoSans(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 15),
+            Text(
+              '중고 거래부터 동네 정보까지, \n지금 내 동네를 선택하고 시작해보세요!',
+              textAlign: TextAlign.center,
+              style: GoogleFonts.notoSans(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+                color: Colors.white.withValues(alpha: 0.6),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
