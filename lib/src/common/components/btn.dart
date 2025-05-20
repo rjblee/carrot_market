4327754b7eaf86da1f0625aaa45fb5007468383c
rjblee <ralph.lee@live.ca>
2025-05-20 23:57:04 +0900

    Edit: Add child and onTap parameters

--- lib/src/common/components/btn.dart
+++ lib/src/common/components/btn.dart
@@ -1,24 +1,20 @@
-import 'package:carrot_market/src/common/components/app_font.dart';
 import 'package:flutter/material.dart';
 
 class Btn extends StatelessWidget {
-  const Btn({super.key});
+  final Widget child;
+  final Function() onTap;
+  const Btn({super.key, required this.child, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {},
+      onTap: onTap,
       child: ClipRRect(
         borderRadius: BorderRadius.circular(7),
         child: Container(
           padding: EdgeInsets.symmetric(vertical: 15),
           color: const Color(0XffED7738),
-          child: AppFont(
-            "시작하기",
-            align: TextAlign.center,
-            size: 18,
-            fontWeight: FontWeight.bold,
-          ),
+          child: child,
         ),
       ),
     );
