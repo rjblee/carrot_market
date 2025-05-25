b00f7cfacb6d881203960fd04a3105d6d88ee2b3
rjblee <ralph.lee@live.ca>
2025-05-25 23:47:09 +0900

    Add: Wrap Obx around child widget to observe changes

--- lib/src/splash/page/splash_page.dart
+++ lib/src/splash/page/splash_page.dart
@@ -1,13 +1,17 @@
+import 'package:carrot_market/src/splash/controller/splash_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends GetView<SplashController> {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Splash 페이지', style: TextStyle(color: Colors.white)),
+        child: Obx(
+          () => Text('Splash 페이지', style: TextStyle(color: Colors.white)),
+        ),
       ),
     );
   }
