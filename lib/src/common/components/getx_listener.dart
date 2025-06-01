900ac02a2600cdee9ff74d0b53f960c826f4538d
rjblee <ralph.lee@live.ca>
2025-06-01 23:53:30 +0900

    Add: GetxListener class

---
+++ lib/src/common/components/getx_listener.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GetxListener<T> extends StatefulWidget {
+  const GetxListener({super.key});
+
+  @override
+  State<GetxListener> createState() => _GetxListenerState();
+}
+
+class _GetxListenerState extends State<GetxListener> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
