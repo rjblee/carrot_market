40f7795427a149ce39859908ab2b56d96f0fd159
rjblee <ralph.lee@live.ca>
2025-06-11 22:44:40 +0900

    Add: Type on loadStep variable as StepType

--- lib/src/splash/controller/splash_controller.dart
+++ lib/src/splash/controller/splash_controller.dart
@@ -2,7 +2,7 @@ import 'package:carrot_market/src/splash/enum/step_type.dart';
 import 'package:get/get.dart';
 
 class SplashController extends GetxController {
-  Rx loadStep = StepType.dataLoad.obs;
+  Rx<StepType> loadStep = StepType.dataLoad.obs;
 
   changeType(StepType type) {
     loadStep(type);
