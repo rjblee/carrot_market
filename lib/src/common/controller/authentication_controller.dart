0e278a1c8961c76859bb8d27043405b357560d3d
rjblee <ralph.lee@live.ca>
2025-08-13 23:57:35 +0900

    Add: Create AuthenticationController class

---
+++ lib/src/common/controller/authentication_controller.dart
@@ -0,0 +1,10 @@
+import 'package:get/get.dart';
+
+class AuthenticationController extends GetxController {
+  RxBool isLogined = false.obs;
+
+  void authCheck() async {
+    await Future.delayed(const Duration(milliseconds: 1000));
+    isLogined(true);
+  }
+}
