589c21c35f7eeb751e51b63448b208c57b7291ca
rjblee <ralph.lee@live.ca>
2025-08-20 23:22:13 +0900

    Add: GetxListener for listening to isLogined

--- lib/src/splash/page/splash_page.dart
+++ lib/src/splash/page/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:carrot_market/src/common/components/getx_listener.dart';
+import 'package:carrot_market/src/common/controller/authentication_controller.dart';
 import 'package:carrot_market/src/common/controller/data_load_controller.dart';
 import 'package:carrot_market/src/splash/controller/splash_controller.dart';
 import 'package:carrot_market/src/splash/enum/step_type.dart';
@@ -14,36 +15,47 @@ class SplashPage extends GetView<SplashController> {
     return Scaffold(
       body: Center(
         child: GetxListener<bool>(
-          listen: (bool value) {
-            if (value) {
-              // true 값이 넘어왔다는 것은 데이터 로드가 완료되었다는 뜻. 그래서 다음 단계인 authCheck 으로 업데이트
-              controller.loadStep(StepType.authCheck);
+          listen: (bool isLogined) {
+            if (isLogined) {
+              Get.offNamed('/home');
+            } else {
+              Get.offNamed('/login');
             }
           },
-          stream: Get.find<DataLoadController>().isDataLoad,
-          child: GetxListener<StepType>(
-            initCall: () {
-              controller.loadStep(
-                StepType.dataLoad,
-              ); // 초기 상태를 업데이트. 여기서는 초기 StepType을 dataLoad로 전달
-            },
-            stream: controller.loadStep, // loadStep 값이 바뀔때마다 listen이 호출됨
-            listen: (StepType? value) {
-              if (value == null) return;
-              switch (value) {
-                case StepType.init:
-                case StepType.dataLoad:
-                  Get.find<DataLoadController>().loadData(); // 데이터 로드를 진행
-                  break;
-                case StepType.authCheck:
-                  print('authCheck');
-                  break;
+          stream: Get.find<AuthenticationController>().isLogined,
+          child: GetxListener<bool>(
+            listen: (bool value) {
+              if (value) {
+                // true 값이 넘어왔다는 것은 데이터 로드가 완료되었다는 뜻. 그래서 다음 단계인 authCheck 으로 업데이트
+                controller.loadStep(StepType.authCheck);
               }
             },
-            child: Obx(
-              () => Text(
-                '${controller.loadStep.value.name}중 입니다.',
-                style: TextStyle(color: Colors.white),
+            stream: Get.find<DataLoadController>().isDataLoad,
+            child: GetxListener<StepType>(
+              initCall: () {
+                controller.loadStep(
+                  StepType.dataLoad,
+                ); // 초기 상태를 업데이트. 여기서는 초기 StepType을 dataLoad로 전달
+              },
+              stream: controller.loadStep, // loadStep 값이 바뀔때마다 listen이 호출됨
+              listen: (StepType? value) {
+                if (value == null) return;
+                switch (value) {
+                  case StepType.init:
+                  case StepType.dataLoad:
+                    Get.find<DataLoadController>().loadData(); // 데이터 로드를 진행
+                    break;
+                  case StepType.authCheck:
+                    Get.find<AuthenticationController>()
+                        .authCheck(); // 인증 체크 진행
+                    break;
+                }
+              },
+              child: Obx(
+                () => Text(
+                  '${controller.loadStep.value.name}중 입니다.',
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
             ),
           ),
