76a3ec478ae8d2d85215323de923e50dcd04b829
rjblee <ralph.lee@live.ca>
2025-09-04 01:16:33 +0900

    Add: Create _SplashPage class

--- lib/src/splash/page/splash_page.dart
+++ lib/src/splash/page/splash_page.dart
@@ -6,6 +6,8 @@ import 'package:carrot_market/src/splash/enum/step_type.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../common/components/app_font.dart';
+
 class SplashPage extends GetView<SplashController> {
   // GetView를 사용하면 위젯 내에서 controller에 바로 접근 가능
   const SplashPage({super.key});
@@ -60,6 +62,44 @@ class SplashPage extends GetView<SplashController> {
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _SplashPage extends GetView<SplashController> {
+  // GetView를 사용하면 위젯 내에서 controller에 바로 접근 가능
+  const _SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const SizedBox(height: 200),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 99,
+                  height: 116,
+                  child: Image.asset('assets/images/logo_simbol.png'),
+                ),
+                const SizedBox(height: 40),
+                AppFont('당신 근처의 밤톨마켓', fontWeight: FontWeight.bold, size: 20),
+
+                const SizedBox(height: 15),
+                AppFont(
+                  '중고 거래부터 동네 정보까지, \n지금 내 동네를 선택하고 시작해보세요!',
+                  align: TextAlign.center,
+                  size: 18,
+                  color: Colors.white.withValues(alpha: 0.6),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
