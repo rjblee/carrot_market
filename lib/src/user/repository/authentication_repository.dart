57674fac7ddda4ac4108fb4eff5eda8ec1144663
rjblee <ralph.lee@live.ca>
2025-09-08 23:35:06 +0900

    Add: Stream to get user's information

--- lib/src/user/repository/authentication_repository.dart
+++ lib/src/user/repository/authentication_repository.dart
@@ -1,7 +1,16 @@
+import 'package:carrot_market/src/user/model/user_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthenticationRepository {
   final FirebaseAuth _firebaseAuth;
 
   AuthenticationRepository(this._firebaseAuth);
+
+  Stream<UserModel?> get user {
+    return _firebaseAuth.authStateChanges().map<UserModel?>((user) {
+      return user == null
+          ? null
+          : UserModel(name: user.displayName, uid: user.uid, email: user.email);
+    });
+  }
 }
