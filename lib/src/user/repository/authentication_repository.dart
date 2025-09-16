143735970958ec4e474e51d2b8103196c266fa1a
rjblee <ralph.lee@live.ca>
2025-09-16 22:53:20 +0900

    Add: signInWithGoogle function

--- lib/src/user/repository/authentication_repository.dart
+++ lib/src/user/repository/authentication_repository.dart
@@ -1,5 +1,6 @@
 import 'package:carrot_market/src/user/model/user_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthenticationRepository {
   final FirebaseAuth _firebaseAuth;
@@ -12,5 +13,16 @@ class AuthenticationRepository {
           ? null
           : UserModel(name: user.displayName, uid: user.uid, email: user.email);
     });
+  }
+
+  Future<void> signInWithGoogle() async {
+    final googleUser = await GoogleSignIn().signIn();
+    final googleAuth = await googleUser?.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    await _firebaseAuth.signInWithCredential(credential);
   }
 }
