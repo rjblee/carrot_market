070006072d03132bbb01303d7ae9819addd03171
rjblee <ralph.lee@live.ca>
2025-09-23 23:52:54 +0900

    Add: Create _authenticationRepository variable and constructor

--- lib/src/common/controller/authentication_controller.dart
+++ lib/src/common/controller/authentication_controller.dart
@@ -1,7 +1,13 @@
+import 'package:carrot_market/src/user/model/user_model.dart';
+import 'package:carrot_market/src/user/repository/authentication_repository.dart';
 import 'package:get/get.dart';
 
 class AuthenticationController extends GetxController {
+  AuthenticationController(this._authenticationRepository);
+
+  final AuthenticationRepository _authenticationRepository;
   RxBool isLogined = false.obs;
+  Rx<UserModel> userModel = const UserModel().obs;
 
   void authCheck() async {
     await Future.delayed(const Duration(milliseconds: 1000));
