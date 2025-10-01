d02f742b667ea2ffca559f8c6053c5d61c332f75
rjblee <ralph.lee@live.ca>
2025-10-01 23:54:30 +0900

    Add: Comments on each enum type

--- lib/src/common/enum/authentication_status.dart
+++ lib/src/common/enum/authentication_status.dart
@@ -1,1 +1,6 @@
-enum AuthenticationStatus { authentication, unAuthenticated, unknown, init }
+enum AuthenticationStatus {
+  authentication, // 로그인 완료 상태
+  unAuthenticated, // Firebase SNS 로그인은 되었지만, 내부 서비스 DB에 가입 이력이 없는 상태
+  unknown, // 비로그인 상태
+  init, // 초기화 상태
+}
