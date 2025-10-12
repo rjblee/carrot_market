ecb7b2836c36e41b2dfc7ac7db4e9dc5a328fbc9
rjblee <ralph.lee@live.ca>
2025-10-12 23:59:44 +0900

    Add: Splash page UI under loginView

--- lib/src/user/login/page/login_page.dart
+++ lib/src/user/login/page/login_page.dart
@@ -4,6 +4,29 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
+  Widget _logoView() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          width: 99,
+          height: 116,
+          child: Image.asset('assets/images/logo_simbol.png'),
+        ),
+        const SizedBox(height: 40),
+        AppFont('당신 근처의 밤톨마켓', fontWeight: FontWeight.bold, size: 20),
+
+        const SizedBox(height: 15),
+        AppFont(
+          '중고 거래부터 동네 정보까지, \n지금 내 동네를 선택하고 시작해보세요!',
+          align: TextAlign.center,
+          size: 18,
+          color: Colors.white.withValues(alpha: 0.6),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: Center(child: const AppFont('로그인')));
